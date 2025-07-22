c4d1289a9e183227071172e7ccc1d26a410207b5
Devendune <devendunegi06@gmail.com>
2025-07-22 09:35:58 +0530

    Marking h2 console as unauthenticated

--- SpringSecurty/src/main/java/com/example/SpringSecurty/GreetingsController.java
+++ SpringSecurty/src/main/java/com/example/SpringSecurty/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.example.SpringSecurty;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingsController
 {
-
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String getHelle()
     {
         return "Hello Buddy";
     }
+
+    @PreAuthorize("/hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint()
+    {
+        return "Hello User buddy";
+    }
+
+    @GetMapping("/admin")
+    public String adminEndPoint()
+    {
+        return "Hello Admin bro";
+    }
+
 
 }
