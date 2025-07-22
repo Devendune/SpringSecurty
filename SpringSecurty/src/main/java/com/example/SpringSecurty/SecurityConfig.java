9eabbf580da5bff4c8a6fcc2ddcfacb0caa145e1
Devendune <devendunegi06@gmail.com>
2025-07-22 23:13:37 +0530

    Changing from inmemory to managing users in h2 Database

--- SpringSecurty/src/main/java/com/example/SpringSecurty/SecurityConfig.java
+++ SpringSecurty/src/main/java/com/example/SpringSecurty/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.example.SpringSecurty;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,19 +21,25 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig
 {
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) ->
-                requests.requestMatchers("/h2/console/**").permitAll()
+                requests.requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated());
-        //http.formLogin(withDefaults());
+        //http.formLogin(withDefaults());D
         http.httpBasic(withDefaults());
+        http.headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(csrf->csrf.disable());
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService()
     {
+
         UserDetails user1= User.withUsername("Devendu2001")
                 .password("{noop}hardworker")
                 .roles("USER")
@@ -39,6 +50,10 @@ public class SecurityConfig
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user1,admin);
+        JdbcUserDetailsManager userDetailsManager=new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+
+        return userDetailsManager;
     }
 }
