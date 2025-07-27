0fd1713c2d063f7faf40f4e220bb88d18c83abd3
Devendune <devendunegi06@gmail.com>
2025-07-27 23:24:53 +0530

    Changes for using own exception and adding Beans

--- SpringSecurty/src/main/java/com/example/SpringSecurty/SecurityConfig.java
+++ SpringSecurty/src/main/java/com/example/SpringSecurty/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.SpringSecurty;
 
+import com.example.securitydemo.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,7 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -26,15 +30,32 @@ public class SecurityConfig
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    private AuthTokenFilter unAuthorizedHandler;
+
+    @Bean
+    private AuthTokenFilter authenticationJwtTokenFilter()
+    {
+        return new AuthTokenFilter();
+    }
+
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) ->
                 requests.requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("api/signin").permitAll()
                         .anyRequest().authenticated());
+
+        http.sessionManagement(session->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         //http.formLogin(withDefaults());D
-        http.httpBasic(withDefaults());
+        //http.httpBasic(withDefaults());
+
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint((AuthenticationEntryPoint) unAuthorizedHandler));
         http.headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         http.csrf(csrf->csrf.disable());
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
