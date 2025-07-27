5a196dfdee13a6c9a460baeb5c88f6f692cbdac5
Devendune <devendunegi06@gmail.com>
2025-07-27 11:49:10 +0530

    Adding more methods

--- SpringSecurty/src/main/java/com/example/securitydemo/jwt/JwtUtils.java
+++ SpringSecurty/src/main/java/com/example/securitydemo/jwt/JwtUtils.java
@@ -1,19 +1,24 @@
 package com.example.securitydemo.jwt;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtils
 {
     //Getting JWT from header
-    private int jwtExpirationMs=30000;
+    private int jwtExpirationMs;
+    private String jwtSecret;
     private static final Logger logger= LoggerFactory.getLogger(JwtUtils.class);
     public String getJwtFromHeader(HttpServletRequest httpServletRequest)
     {
@@ -34,11 +39,24 @@ public class JwtUtils
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date((new Date().getTime()+jwtExpirationMs)))
-                .signWith(key)
+                .signWith(key())
                 .compact();
     }
 
-    //
+    //Getting Username from Token
+    public String generateUserNameFromToken(String token)
+    {
+        return Jwts.parser()
+                .verifyWith((SecretKey)key())
+                .build().parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
+
+    public Key key()
+    {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
 
 
 }
